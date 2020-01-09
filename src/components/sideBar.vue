512d1b2d030e64872ddb5a73f625daf9a665be4b
unknown <rodrigodlq@hotmail.com>
2020-01-09 15:42:15 +0000

    Sidebar

--- src/components/sideBar.vue
+++ src/components/sideBar.vue
@@ -1,36 +1,57 @@
 <template>
-  <aside class="menu">
-  <p class="menu-label">
+  <aside class="menu column is-2 section isSecondaryBGColor is-fullheight SideBar">
+    <p class="is-size-4 isPrimaryColor has-text-centered">Admin</p>
+  <p class="menu-label pWhite is-size-6">
     General
   </p>
   <ul class="menu-list">
-    <li><a>Dashboard</a></li>
-    <li><a>Customers</a></li>
+    <li><a class="aWhite" >Dashboard</a></li>
   </ul>
-  <p class="menu-label">
+  <p class="menu-label pWhite is-size-6">
     Administration
   </p>
   <ul class="menu-list">
-    <li><a>Team Settings</a></li>
-    <li>
-      <a class="is-active">Manage Your Team</a>
-      <ul>
-        <li><a>Members</a></li>
-        <li><a>Plugins</a></li>
-        <li><a>Add a member</a></li>
-      </ul>
-    </li>
-    <li><a>Invitations</a></li>
-    <li><a>Cloud Storage Environment Settings</a></li>
-    <li><a>Authentication</a></li>
+    <li><a class="aWhite" >Validate installation requests</a></li>
+    <li><a class="aWhite" >Subscription packages</a></li>
+    <li><a class="aWhite" >Gamification</a></li>
   </ul>
-  <p class="menu-label">
-    Transactions
+  <p class="menu-label pWhite is-size-6">
+    Sensors
   </p>
   <ul class="menu-list">
-    <li><a>Payments</a></li>
-    <li><a>Transfers</a></li>
-    <li><a>Balance</a></li>
+    <li><a class="aWhite" >New Sensor</a></li>
+    <li><a class="aWhite" >Manage Sensors</a></li>
+    <li><a class="aWhite" >Orders</a></li>
   </ul>
+  <p class="menu-label pWhite is-size-6">
+    Users
+  </p>
+  <ul class="menu-list">
+    <li><a class="aWhite" >Manage Users</a></li>
+  </ul>
+  <br>
+  <p class="menu-label pWhite is-size-6">
+    <i class="fas fa-sign-out-alt"></i> Sign out
+  </p>
 </aside>
 </template>
+
+<style >
+
+.pWhite {
+  color: hsl(0, 0%, 96%) !important;
+}
+
+.aWhite {
+  color: hsl(0, 0%, 70%) !important;
+}
+
+.SideBar {
+  height: 100vh;
+  position: fixed;
+  top: 0px;
+  left: 0px;
+  z-index: 10;
+}
+
+</style>
