706ac5bef532aa348a37e0daddaf35d6a8c27faa
unknown <rodrigodlq@hotmail.com>
2020-01-09 15:26:13 +0000

    changes

---
+++ src/views/DashBoard.vue
@@ -0,0 +1,52 @@
+<template>
+<body>
+
+    <SideBar></SideBar>
+    <section class="isWhiteBG column is-10 is-offset-2">
+        
+        <nav class="level is-mobile">
+  <div class="level-item has-text-centered box">
+    <div>
+      <p class="heading">Users</p>
+      <p class="title">3,456</p>
+    </div>
+  </div>
+  <div class="level-item has-text-centered box">
+    <div>
+      <p class="heading">Sensors Sold</p>
+      <p class="title">123</p>
+    </div>
+  </div>
+  <div class="level-item has-text-centered box">
+    <div>
+      <p class="heading">Installation Requests</p>
+      <p class="title">456K</p>
+    </div>
+  </div>
+  <div class="level-item has-text-centered box">
+    <div>
+      <p class="heading">Orders</p>
+      <p class="title">789</p>
+    </div>
+  </div>
+</nav>
+
+
+    </section>
+        
+   
+</body>
+    
+
+</template>
+
+<script>
+import SideBar from "../components/sideBar"
+
+
+export default {
+    components: {
+    SideBar, 
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,72 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <!-- is-fullheight-with-navbar -->
+  <body>
+    <section class="hero isWhiteBG is-fullheight patternBG">
+        <div class="hero-body">
+            <div class="container has-text-centered">
+                <div class="column is-4 is-offset-4">                    
+
+                    <div class="box isSecondaryBGColor">
+                      <figure class="avatar">
+                            <img src="../assets/Images/1.png" width="300" >
+                        </figure>
+                        <br>
+                        <br>
+                        <form>
+                            <div class="field">
+                                <div class="control">
+                                    <input v-model="email" class="input" type="email" placeholder="Your Email" autofocus="">
+                                </div>
+                            </div>
+
+                            <div class="field">
+                                <div class="control">
+                                    <input v-model="password" class="input" type="password" placeholder="Your Password">
+                                </div>
+                            </div>
+                           <br>
+                           <br>
+                            <button @click="goTo('/dashboard')" class="button is-block isPrimaryBGColor is-medium is-fullwidth">Login <i class="fas fa-sign-in-alt"></i></button>
+                        </form>
+                    </div>
+                    <p class="has-text-grey">
+                        <a href="../">Sign Up</a> &nbsp;·&nbsp;
+                        <a href="../">Forgot Password</a> &nbsp;·&nbsp;
+                        <a href="../">Need Help?</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+  </body>
+  
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: "home",
-  components: {
-    HelloWorld
+  name: "Home",
+  data() {
+    return {
+      email: "",
+      password: ""
+    };
+  },
+  created() {
+    
+  },
+  methods: {
+    goTo(path) {
+      this.$router.push(path);
+    }
   }
-};
+}
 </script>
+
+<style>
+.patternBG {
+  background-image: url("../assets/Images/pattern2.png")
+}
+</style>
