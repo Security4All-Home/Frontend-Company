d22eba4e9e9a4d274b21722fe32b41ccd04860ed
hols <adholstein@gmail.com>
2020-01-19 22:45:47 +0000

    more heroku

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,5 @@
+require("dotenv").config()
+console.log(rocess.env.production, "rocess.env.production")
 export default {
-    BASE_URL:'http://localhost:8002',
+    BASE_URL: process.env.production ? "https://sec4allapp.herokuapp.com/home" :'http://localhost:8002'
  }
